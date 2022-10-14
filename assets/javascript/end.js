78e43510edd4c3d4636e196eab8efc08a547f82c
seanf316 <seanfinn316@gmail.com>
2022-10-14 21:56:50 +0000

    Added some criteria for username length

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -1,22 +1,21 @@
 const username = document.querySelector('#username');
+const usernameText = document.querySelector('#username-text');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
-const MAX_HIGH_SCORES = 10;
-
-
-
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerHTML = `You corectly answered ${mostRecentScore} out of 10`;
 
 username.addEventListener('keyup', () => {
-    if (username.value >= 8) {
-        alert("Please enter a username less then 8 characters")
-    }
-    saveScoreBtn.disabled = !username.value
+    if (username.value.length > 8) {
+        usernameText.innerText = "Please enter a username less then 8 characters"
+        saveScoreBtn.disabled = true
+    } else {
+        usernameText.innerText = ""
+        saveScoreBtn.disabled = false
+    }    
 });
 
 saveHighScore = e => {
