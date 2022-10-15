b00d39841fb5bfd6273ddf4c2b5cbad5eaa4e895
seanf316 <seanfinn316@gmail.com>
2022-10-15 00:31:07 +0000

    Add some text and css to end.html

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -3,6 +3,7 @@ const usernameText = document.querySelector('#username-text');
 const _goHome = document.getElementById('goHome');
 const _endForm = document.getElementById('end-form');
 const _finalText = document.getElementById('final-text');
+const _finalQuote = document.getElementById('final-quote');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
@@ -19,7 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
 function replayQuiz() {
     if (mostRecentScore == 0) {
         _endForm.style.display = 'none';
-        _finalText.innerText = "Sorry you didn't answer any questions correctly you will need to go again!"
+        _finalText.innerHTML = `Sorry my friend you answered <strong>0</strong> questions correctly, not everyone is cursed with knowledge.<br>Play again to get your name on the Highscores Table.`
+        _finalQuote.innerHTML = `“Everybody looses a couple, and you either pack up and go home or keep fighting.” - Seabiscuit 2003`
     } else {
         finalScore.innerHTML = `You corectly answered ${mostRecentScore} out of 10`;
     }
