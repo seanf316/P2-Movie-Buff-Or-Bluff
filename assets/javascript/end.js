2f9e0ccb7de99ec9c71b75f4d2432850e96686d9
seanf316 <seanfinn316@gmail.com>
2022-10-15 01:01:14 +0000

    Clean up js code in end.html

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -2,6 +2,7 @@ const username = document.querySelector('#username');
 const usernameText = document.querySelector('#username-text');
 const _goHome = document.getElementById('goHome');
 const _endForm = document.getElementById('end-form');
+const _finalTextContainer = document.getElementById('finalTextContainer');
 const _finalText = document.getElementById('final-text');
 const _finalQuote = document.getElementById('final-quote');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
@@ -11,25 +12,13 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const MAX_HIGH_SCORES = 5;
 
 
-
-
+// Event Listeners
+username.addEventListener('keyup', usernameEnter);
 document.addEventListener('DOMContentLoaded', () => {
     replayQuiz();
   })
 
-function replayQuiz() {
-    if (mostRecentScore == 0) {
-        _endForm.style.display = 'none';
-        _finalText.innerHTML = `Sorry my friend you answered <strong>0</strong> questions correctly, not everyone is cursed with knowledge.<br>Play again to get your name on the Highscores Table.`
-        _finalQuote.innerHTML = `“Everybody looses a couple, and you either pack up and go home or keep fighting.” - Seabiscuit 2003`
-    } else {
-        finalScore.innerHTML = `You corectly answered ${mostRecentScore} out of 10`;
-    }
-}
-
-
-
-username.addEventListener('keyup', () => {
+  function usernameEnter() {
     if (username.value.length < 4) {
         usernameText.innerText = "Please enter a username between 4 & 10 characters"
         saveScoreBtn.disabled = true
@@ -40,15 +29,22 @@ username.addEventListener('keyup', () => {
         usernameText.innerText = ""
         saveScoreBtn.disabled = false   
     }
-    // else if (username.value.length > 10) {
-    //     usernameText.innerText = "Please enter a username between 4 & 10 characters"
-    //     saveScoreBtn.disabled = true
-    // }
-    
-});
+}
 
-saveHighScore = e => {
-    e.preventDefault()
+function replayQuiz() {
+    if (mostRecentScore == 0) {
+        _endForm.style.display = 'none';
+        _finalText.innerHTML = `Sorry my friend you answered <strong>0</strong> questions correctly, not everyone is cursed with knowledge.<br>Play again to get your name on the Highscores Table.`
+        _finalQuote.innerHTML = `“Everybody looses a couple, and you either pack up and go home or keep fighting.” - Seabiscuit 2003`
+    } else {
+        finalScore.innerHTML = `You corectly answered ${mostRecentScore} out of 10`;
+        _finalTextContainer.style.display = 'none';
+        usernameEnter();
+    }
+}
+
+function saveHighScore (event) {
+    event.preventDefault()
 
     const score = {
         score: mostRecentScore,
@@ -64,4 +60,4 @@ saveHighScore = e => {
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
     window.location.assign('/')
-};
+}
