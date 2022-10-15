b86255a46bf3f4974841d6465a1c7ea2e2e7d0c3
seanf316 <seanfinn316@gmail.com>
2022-10-15 00:12:12 +0000

    Add function to hide username input if user score is 0

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -1,21 +1,48 @@
 const username = document.querySelector('#username');
 const usernameText = document.querySelector('#username-text');
+const _goHome = document.getElementById('goHome');
+const _endForm = document.getElementById('end-form');
+const _finalText = document.getElementById('final-text');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const MAX_HIGH_SCORES = 5;
 
-finalScore.innerHTML = `You corectly answered ${mostRecentScore} out of 10`;
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    replayQuiz();
+  })
+
+function replayQuiz() {
+    if (mostRecentScore == 0) {
+        _endForm.style.display = 'none';
+        _finalText.innerText = "Sorry you didn't answer any questions correctly you will need to go again!"
+    } else {
+        finalScore.innerHTML = `You corectly answered ${mostRecentScore} out of 10`;
+    }
+}
+
+
 
 username.addEventListener('keyup', () => {
-    if (username.value.length > 8) {
-        usernameText.innerText = "Please enter a username less then 8 characters"
+    if (username.value.length < 4) {
+        usernameText.innerText = "Please enter a username between 4 & 10 characters"
+        saveScoreBtn.disabled = true
+    } else if (username.value.length >= 11) {
+        usernameText.innerText = "Please enter a username between 4 & 10 characters"
         saveScoreBtn.disabled = true
     } else {
         usernameText.innerText = ""
-        saveScoreBtn.disabled = false
-    }    
+        saveScoreBtn.disabled = false   
+    }
+    // else if (username.value.length > 10) {
+    //     usernameText.innerText = "Please enter a username between 4 & 10 characters"
+    //     saveScoreBtn.disabled = true
+    // }
+    
 });
 
 saveHighScore = e => {
--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -5,7 +5,7 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 highScoresList.innerHTML =
 highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join('');
+}).join('')
 
 _clearBtn.addEventListener('click', removeHighscores);
 
